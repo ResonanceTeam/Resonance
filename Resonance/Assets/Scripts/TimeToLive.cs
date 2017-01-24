5c822f1e677343ebf77897268565ece3be91f665
Thomas Kuharski <TomPK7@gmail.com>
2017-01-24 00:22:48 -0600

    Created project, added assets
    Created new unity project, exported all assets from the vr0 version and
    imported them to the new project.

---
+++ Resonance/Assets/Scripts/TimeToLive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeToLive : MonoBehaviour {
+
+	public float maxTime;
+	private float currentTime;
+	// Use this for initialization
+	void Start () {
+		currentTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTime += Time.deltaTime;
+		if (currentTime > maxTime) {
+			Destroy(gameObject);
+		}
+	}
+}
